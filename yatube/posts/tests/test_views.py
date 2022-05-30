362fee35e91e84a3c4aedf52e993b6ffeb364556
Вадим Андрианов <vadim.andrianov.new@yandex.ru>
2022-05-30 23:37:52 +0300

    final homework

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -6,9 +6,9 @@ from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
-from ..models import Group, Post, Follow
+from ..models import Group, Post
 from django import forms
-from ..forms import PostForm, Comment
+from ..forms import PostForm, Comment, Follow
 from django.core.cache import cache
 
 User = get_user_model()
