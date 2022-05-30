917c0a87cc2039d4cb98e5766cfc773533b24dfb
Вадим Андрианов <vadim.andrianov.new@yandex.ru>
2022-05-31 00:00:02 +0300

    last work

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -12,3 +12,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+
+
+class FollowForm(forms.ModelForm):
+    class Meta:
+        model = Follow
+        fields = ()
